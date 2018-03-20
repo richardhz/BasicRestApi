aba122b47cabaa29a073591ce6fa68a60b68acdb
Richard Hernandez <r.a.hernandez@outlook.com>
2018-03-20 15:35:15 +0000

    Add project files.

---
+++ RestApiCore/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestApiCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Service")]
+    public class ServiceController : Controller
+    {
+        private readonly ILogger _logger;
+        public ServiceController(ILogger<ServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Status")]
+        [Produces(typeof(object))]
+        public IActionResult Status()
+        {
+            var appName = Assembly.GetEntryAssembly().GetName().Name;
+            var appVersion = Assembly.GetEntryAssembly().GetName().Version.ToString();
+
+            var info = new { ApplicationName = appName, AppVersion = appVersion, Status = "Running" };
+            return Ok(info);
+        }
+    }
+}
---
+++ RestApiCore/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace RestApiCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Basic REST API", Version = "V1" });
+            });
+
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.Formatting = Formatting.Indented;
+                });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context => {
+                        var ExceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (ExceptionHandlerFeature != null)
+                        {
+                            logger.LogError(ExceptionHandlerFeature.Error.Message);
+                        }
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("Something is wrong. Please try again later.");
+                    });
+                });
+            }
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basic REST API V1");
+            });
+
+        }
+    }
+}
